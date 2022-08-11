07c2b78856c2dc2b3139755431de6e6354ef3444
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-08-11 21:49:44 +0100

    update 4

---
+++ coffee-sub/src/components/txt.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "../index.css";
+
+export default class text1 extends Component{
+    render(){
+        return(
+            <div>
+                <h3 id='txt-1'>Start your subscription Today.</h3>
+                <h3 id='txt-2'>Starting at £4.99 monthly!</h3>
+            </div>
+        )
+    }
+}
