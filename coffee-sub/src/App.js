f07f1026457b05db9a0deadf4656ef62eb034919
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-08-11 21:25:59 +0100

    update 2

---
+++ coffee-sub/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import Background from './components/bg'
+
+export default class App extends Component{
+    render(){
+        return{
+            <div>
+                <Background/>
+            </div>
+        };
+    }
+}
---
+++ coffee-sub/src/components/bg.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import bg from "../images/coffee-bg.jpg";
+import "../index.css";
+
+export default class Bg extends Component {
+    render(){
+        return {
+            <div>
+                <img src={bg}></img>
+                <h2></h2>
+            </div>
+        };
+    }
+}
