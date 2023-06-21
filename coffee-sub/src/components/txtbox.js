906b6b3f3242d6c6de3cc4851a5c1658f83797e3
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-06-21 17:47:43 +0100

    new component

---
+++ coffee-sub/src/components/txtbox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "../index.css";
+
+export default class txtBox extends Component{
+    render(){
+        return(
+            <div className='txtBox-div'>
+
+            </div>
+        )
+    }
+}
