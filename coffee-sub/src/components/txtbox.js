ac6e8a32a3ee8bacd519279b7064cd45a9973c87
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-06-21 18:16:51 +0100

    adding text and styling

--- coffee-sub/src/components/txtbox.js
+++ coffee-sub/src/components/txtbox.js
@@ -5,7 +5,9 @@ export default class txtBox extends Component{
     render(){
         return(
             <div className='txtBox-div'>
-                <span className=''>hello</span>
+                <span className='txtBox-txt'>Coffee can help people feel less tired and increase
+                energy levels. That's because it contains a stimulant called caffeine the 
+                most commonly consumed psychoactive substance in the world.</span>
             </div>
         )
     }
