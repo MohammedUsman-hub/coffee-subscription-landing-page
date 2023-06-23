6a5c8661c5f74b623633175adbe450825759f822
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-06-23 19:29:20 +0100

    codded sm logos

--- coffee-sub/src/components/SmLogo.js
+++ coffee-sub/src/components/SmLogo.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import '../index.css';
 
+import Facebook from '../images/fb.png'
+import Instagram from '../images/insta.png'
+import Twitter from '../images/tw.png'
+
 export default class SmLogo extends Component{
     render(){
         return(
-            <div>
-                
+            <div className='smLogo'>
+                <img className='fb' src={Facebook}></img>
+                <img className='insta' src={Instagram}></img>
+                <img className='tw' src={Twitter}></img>
             </div>
         )
     }
