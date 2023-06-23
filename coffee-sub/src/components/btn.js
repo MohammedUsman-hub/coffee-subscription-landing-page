836483274cd75a48f8b176de5540686e4265ea74
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-06-23 19:14:01 +0100

    ...

--- coffee-sub/src/components/btn.js
+++ coffee-sub/src/components/btn.js
@@ -5,7 +5,7 @@ export default class JoinBtn extends Component{
     render(){
         return(
             <div classname="button-div">
-                <b><button id='join-btn'>Join</button></b>
+                <b><button id='join-btn'>Sign Up</button></b>
             </div>
 
             
