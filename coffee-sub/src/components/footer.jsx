0b1e752649acb2595342ab0b40d1947676ebda82
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-06-24 17:37:10 +0100

    adding footer

---
+++ coffee-sub/src/components/footer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'    
+import '..index.css/'
+
+const Footer = () => {
+    const year = new Date().getFullYear();
+  
+    return <footer>{`Copyright © Upbeat Code ${year}`}</footer>;
+  };
+  
+  export default Footer;
